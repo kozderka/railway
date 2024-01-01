058c5c1c706590c90263f8696b4cc465be027fc2
Michal Kozderka <kozderkamichal@gmail.com>
2024-01-01 22:28:30 +0100

    test result creationů

--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,31 @@
 import { describe, test } from 'node:test'
 import assert from 'node:assert'
-import { FAILURE, SUCCESS, failure, success } from '../src/index.js'
+import { FAILURE, SUCCESS, failure, isSuccess, success } from '../src/index.js'
 
-describe('Railway', () => {
+describe('Result creation', () => {
   test('should be success', () => {
     const result = success('success')
+
     assert.strictEqual(result.tag, SUCCESS)
     assert.strictEqual(result.value, 'success')
   })
   test('should be failure', () => {
     const result = failure('failure')
+
     assert.strictEqual(result.tag, FAILURE)
     assert.strictEqual(result.error, 'failure')
+  })
+})
+
+describe('Helpers', () => {
+  test('isSuccess should return true', () => {
+    const result = success('success')
+
+    assert.strictEqual(isSuccess(result), true)
+  })
+  test('isSuccess should return false', () => {
+    const result = failure('success')
+
+    assert.strictEqual(isSuccess(result), false)
   })
 })
