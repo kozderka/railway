6bf5fe82d7cd36ec168229ee141eb76cade0c79c
Michal Kozderka <kozderkamichal@gmail.com>
2024-01-02 21:25:55 +0100

    testing helpers

--- test/index.test.js
+++ test/index.test.js
@@ -1,15 +1,15 @@
 import { describe, test } from 'node:test'
 import assert from 'node:assert'
-import { FAILURE, SUCCESS, failure, isSuccess, success } from '../src/index.js'
+import { FAILURE, SUCCESS, chain, failure, getValue, isFailure, isSuccess, map, success, tee } from '../src/index.js'
 
 describe('Result creation', () => {
-  test('should be success', () => {
+  test('result should be success', () => {
     const result = success('success')
 
     assert.strictEqual(result.tag, SUCCESS)
     assert.strictEqual(result.value, 'success')
   })
-  test('should be failure', () => {
+  test('result should be failure', () => {
     const result = failure('failure')
 
     assert.strictEqual(result.tag, FAILURE)
@@ -27,5 +27,38 @@ describe('Helpers', () => {
     const result = failure('success')
 
     assert.strictEqual(isSuccess(result), false)
+  })
+  test('isFailure should return true', () => {
+    const result = failure('failure')
+
+    assert.strictEqual(isFailure(result), true)
+  })
+  test('isFailure should return false', () => {
+    const result = success('failure')
+
+    assert.strictEqual(isFailure(result), false)
+  })
+  test('value should be success', () => {
+    const result = success('success')
+
+    assert.strictEqual(getValue(result), 'success')
+  })
+  test('result should by maped to success', () => {
+    const result = map((value) => value + ' mapped')(1)
+
+    assert.strictEqual(result.tag, SUCCESS)
+    assert.strictEqual(result.value, '1 mapped')
+  })
+  test('chained result should be success', () => {
+    const result = chain((value) => success(value + ' chained'))(success('success'))
+
+    assert.strictEqual(result.tag, SUCCESS)
+    assert.strictEqual(result.value, 'success chained')
+  })
+  test('tee should return value from parameter', () => {
+    const result = tee((value) => { return value + ' tee' })(success('success'))
+
+    assert.strictEqual(result.tag, SUCCESS)
+    assert.strictEqual(result.value, 'success')
   })
 })
